f7fcd75386c21580b2c96275cbc8f96f81495bd1
Andy Chosak <andy.chosak@cfpb.gov>
2022-02-25 16:40:57 -0500

    Bumping version of https://deno.land/x/flat
    Trying to fix nightly job failures

--- postprocess.js
+++ postprocess.js
@@ -1,6 +1,6 @@
 import prettify from 'https://dev.jspm.io/prettify-xml';
 
-import { readTXT, writeTXT, removeFile } from 'https://deno.land/x/flat@0.0.11/mod.ts'
+import { readTXT, writeTXT, removeFile } from 'https://deno.land/x/flat@0.0.14/mod.ts'
 
 const xml = await readTXT('sitemap.xml');
 
